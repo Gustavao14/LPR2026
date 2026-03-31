76046ac59e08827062fc52523f2b7eaa1f9ba201
Gustavao14 <gustavofernandes262027@gmail.com.br>
2026-03-31 09:53:35 -0300

    aula3atualizacao

--- Aula2/Exercicio4/obj/Debug/net9.0/Exercicio4.AssemblyInfo.cs
+++ Aula2/Exercicio4/obj/Debug/net9.0/Exercicio4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f4fcfe2ac866d81a75f29ea57ba7225668f4fc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
